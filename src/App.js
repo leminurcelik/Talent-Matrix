571c7bb9ab74013382dfd3d16f1af3426e823353
leminurcelik <celikleminur@gmail.com>
2023-07-27 16:29:45 +0300

    Modify application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navigation from './Navigation';
+import MatrixType from './MatrixType';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full">
+      <Navigation label="Yetenek Matrisi" />
+      <MatrixType title="Matris Tipi"/>
     </div>
   );
 }
-
-export default App;
